2ba452392ee3b60dd5415569dffec40d0342b804
Adam Borys <hipowolemia@gmail.com>
2018-05-07 22:55:02 +0200

    Update server.js
    Poprawiony zgodnie z index

--- server.js
+++ server.js
@@ -1,91 +1,91 @@
 //Dependencies
-var express = require('express'); // Express contains some boilerplate to for routing and such
+var express = require('express'); // Express dołączony dla potrzeb routingu
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http); // Here's where we include socket.io as a node module 
+var io = require('socket.io')(http); // socket io dołączamy jako node'owy moduł 
 
-// Serve the index page 
+// Obsługa index.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/index.html'); 
 });
 
-// Serve the assets directory
+// Obsługa folderu assetów
 app.use('/assets',express.static('assets'))
 
-// Listen on port 5000
+// Nasłuchiwanie na 5000
 app.set('port', (process.env.PORT || 5000));
 http.listen(app.get('port'), function(){
   console.log('listening on port',app.get('port'));
 });
 
-var players = {}; //Keeps a table of all players, the key is the socket id
-var bullet_array = []; // Keeps track of all the bullets to update them on the server 
-// Tell Socket.io to start accepting connections
+var players = {}; //Tablica graczy identyfikowanych po socket id
+var ball_array = []; // Tablica do śledzenia pocisków 
+// Włączamy akceptowanie połączeń
 io.on('connection', function(socket){
-	// Listen for a new player trying to connect
+	// Nasłuchiwanie nowych graczy
 	socket.on('new-player',function(state){
 		console.log("New player joined with state:",state);
 		players[socket.id] = state;
-		// Broadcast a signal to everyone containing the updated players list
+		// Sygnał broadcast aktualizujący liczbę graczy w klientach
 		io.emit('update-players',players);
 	})
   
-  // Listen for a disconnection and update our player table 
+  // Nasłuchiwanie rozłączania graczy i aktualizacja tabeli
   socket.on('disconnect',function(state){
     delete players[socket.id];
     io.emit('update-players',players);
   }) 
   
-  // Listen for move events and tell all other clients that something has moved 
+  // Nasłuchiwanie ruchu oraz informowanie o nim klientów
   socket.on('move-player',function(position_data){
-    if(players[socket.id] == undefined) return; // Happens if the server restarts and a client is still connected 
+    if(players[socket.id] == undefined) return; // Wykluczenie przypadków gdy klient jest wciąż połączony po restarcie serwera 
     players[socket.id].x = position_data.x;  
     players[socket.id].y = position_data.y; 
     players[socket.id].angle = position_data.angle; 
     io.emit('update-players',players);
   })
   
-  // Listen for shoot-bullet events and add it to our bullet array
-  socket.on('shoot-bullet',function(data){
+  // Nasłuchiwanie wystrzałów (shoot-ball) oraz dodawanie do tablicy
+  socket.on('shoot-ball',function(data){
     if(players[socket.id] == undefined) return;
-    var new_bullet = data;
-    data.owner_id = socket.id; // Attach id of the player to the bullet 
+    var new_ball = data;
+    data.owner_id = socket.id; // Dołączanie id właściciela do pocisku
     if(Math.abs(data.speed_x) > 20 || Math.abs(data.speed_y) > 20){
-      console.log("Player",socket.id,"is cheating!");
-    }
-    bullet_array.push(new_bullet);
+      console.log("Player",socket.id,"oszukuje!");
+    } // Bat na cwaniaków hackujących naszą grę
+    ball_array.push(new_ball);
   });
 })
 
-// Update the bullets 60 times per frame and send updates 
+// Aktualizowanie pocisków ze stosowną szybkością (około 60 klatek na sekundę) 
 function ServerGameLoop(){
-  for(var i=0;i<bullet_array.length;i++){
-    var bullet = bullet_array[i];
-    bullet.x += bullet.speed_x; 
-    bullet.y += bullet.speed_y; 
+  for(var i=0;i<ball_array.length;i++){
+    var ball = ball_array[i];
+    ball.x += ball.speed_x; 
+    ball.y += ball.speed_y; 
     
-    // Check if this bullet is close enough to hit any player 
+    // Sprawdzanie czy pocisk uderza gracza
     for(var id in players){
-      if(bullet.owner_id != id){
-        // And your own bullet shouldn't kill you
-        var dx = players[id].x - bullet.x; 
-        var dy = players[id].y - bullet.y;
+      if(ball.owner_id != id){
+        // i wykluczenie sytuacji, gdy jest to jego własny (np. przy wystrzale)
+        var dx = players[id].x - ball.x; 
+        var dy = players[id].y - ball.y;
         var dist = Math.sqrt(dx * dx + dy * dy);
         if(dist < 70){
-          io.emit('player-hit',id); // Tell everyone this player got hit
+          io.emit('player-hit',id); // Broadcast, że gracz został uderzony
         }
       }
     }
     
-    // Remove if it goes too far off screen 
-    if(bullet.x < -10 || bullet.x > 1000 || bullet.y < -10 || bullet.y > 1000){
-        bullet_array.splice(i,1);
+    // Usuwanie pocisków spoza obszaru gry 
+    if(ball.x < -10 || ball.x > 1000 || ball.y < -10 || ball.y > 1000){
+        ball_array.splice(i,1);
         i--;
     }
         
   }
-  // Tell everyone where all the bullets are by sending the whole array
-  io.emit("bullets-update",bullet_array);
+  // Broadcast tablicy z pozycjami pocisków
+  io.emit("balls-update",ball_array);
 }
 
 setInterval(ServerGameLoop, 16); 
